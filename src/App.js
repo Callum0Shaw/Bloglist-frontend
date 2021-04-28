42e8fcad0fbfcdbb33281d5730b630b1546c25a7
Your Name <callum0shaw.gmail.com>
2021-04-28 20:08:08 +0300

    SUBMIT: Exercise 5.2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import Login from "./components/Login";
+import Logout from "./components/Logout";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 
@@ -18,19 +19,31 @@ const App = () => {
     fetchBlogs();
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedUser")
+    if (loggedUserJSON) {
+      const currentUser = JSON.parse(loggedUserJSON)
+      setUser(currentUser)
+      blogService.setToken(currentUser.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault();
     console.log("Logging in with ", username, password);
     try {
-      const newUser = await loginService.login({ username, password });
-      blogService.setToken(newUser.token);
-      setUser(newUser);
+      const currentUser = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedUser", JSON.stringify(currentUser));
+      blogService.setToken(currentUser.token);
+      setUser(currentUser);
       setUsername("");
       setPassword("");
     } catch (error) {
       alert("Wrong login details");
     }
   };
+
+
   if (user === null) {
     return (
       <div>
@@ -49,7 +62,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <Logout name={user.name}/>
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
