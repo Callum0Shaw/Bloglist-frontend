586c75cb7d0e23c7abf7b00c908f0438f941c296
Your Name <callum0shaw.gmail.com>
2021-05-08 20:41:45 +0300

    SUBMIT: Exercise 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,24 +1,26 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
-import { render } from "@testing-library/react"
+import { render, fireEvent } from "@testing-library/react"
 import Blog from "./Blog"
 
-test("Blog renders content", () => {
-  const blog = {
-    title: "Test Title",
-    author: "Test Author",
-    url: "Test Url",
-    Likes: 5,
-  }
-  const user = {
-    id: 1,
-    name: "Test Name",
-    username: "Test Username",
-  }
+const user = {
+  id: 1,
+  name: "Test Name",
+  username: "Test Username",
+}
 
-  const likeBlog = jest.fn()
-  const deleteBlog = jest.fn()
+const blog = {
+  title: "Test Title",
+  author: "Test Author",
+  url: "Test Url",
+  likes: 5,
+  user: user,
+}
 
+const likeBlog = jest.fn()
+const deleteBlog = jest.fn()
+
+test("Minimised blog shows correct content", () => {
   const component = render(
     <Blog blog={blog} likeBlog={likeBlog} deleteBlog={deleteBlog} user={user} />
   )
@@ -26,5 +28,19 @@ test("Blog renders content", () => {
   expect(component.container).toHaveTextContent(
     `${blog.title} by ${blog.author}`
   )
-  expect(component.container).not.toHaveTextContent("Test Url", "5")
+  expect(component.container).not.toHaveTextContent(blog.url, blog.likes)
+})
+
+test("Expanded blog show correct content", () => {
+  const component = render(
+    <Blog blog={blog} likeBlog={likeBlog} deleteBlog={deleteBlog} user={user} />
+  )
+
+  const showButton = component.getByText("View")
+
+  fireEvent.click(showButton)
+
+  expect(component.container).toHaveTextContent(
+    blog.title, blog.author, blog.url, blog.likes
+  )
 })
